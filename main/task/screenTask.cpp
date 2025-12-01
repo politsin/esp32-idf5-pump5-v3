ed5a9ff1d464539dd14fb4856d89b379f5c81bc9
Anatoly Politsin <politsin@gmail.com>
2025-12-01 20:35:48 +0300

    tweak(screen): средний индикатор (FLUSH) окрашен в синий на экране

--- main/task/screenTask.cpp
+++ main/task/screenTask.cpp
@@ -163,7 +163,8 @@ void screenTask(void *pvParam) {
   };
   // Индикаторы статусов слева направо: Зелёный (START/RUN), Жёлтый (FLUSH), Красный (STOP)
   lv_obj_t *sqGreen  = createFig(lv_color_hex(0x00FF00), btnX_start);
-  lv_obj_t *sqYellow = createFig(lv_color_hex(0xFFFF00), btnX_start + btnSpacing);
+  // Средний индикатор (FLUSH) делаем СИНИМ на экране (с учётом текущего цветопорядка)
+  lv_obj_t *sqYellow = createFig(lv_color_hex(0xFF0000), btnX_start + btnSpacing);
   // Под текущее отображение цветов: используем код, который даст на экране красный
   lv_obj_t *sqRed    = createFig(lv_color_hex(0x0000FF), btnX_start + btnSpacing * 2);
   auto setWeak = [](lv_obj_t *obj){
